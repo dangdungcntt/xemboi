9f9cc0e36fec49ae94a5afa580472b7a543bf447
DangDung <dangdungcntt@gmail.com>
2017-12-27 11:36:11 +0700

    send meta tag for facebot

--- apps/controllers/cat.js
+++ apps/controllers/cat.js
@@ -12,7 +12,7 @@ const ucfirst = str => str.charAt(0).toUpperCase() + str.slice(1);
 
 router.get('*', (req, res) => {
   const url = 'http://iwanttodrawacatforyou.com/wp-content/uploads/';
-  res.redirect(`${url}${Math.floor(Math.random() * 20000) + 100}.jpg`);
+  res.send(`<meta property="og:url" content="${url}${Math.floor(Math.random() * 20000) + 100}.jpg" />`);
 });
 
 module.exports = router;
