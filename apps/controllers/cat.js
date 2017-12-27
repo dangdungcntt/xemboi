16ae3293a9e4bdca32600985944be04509ac7273
DangDung <dangdungcntt@gmail.com>
2017-12-27 12:29:31 +0700

    change to og:imgae

--- apps/controllers/cat.js
+++ apps/controllers/cat.js
@@ -12,7 +12,7 @@ const ucfirst = str => str.charAt(0).toUpperCase() + str.slice(1);
 
 router.get('/:t', (req, res) => {
   const url = 'http://iwanttodrawacatforyou.com/wp-content/uploads/';
-  res.send(`<meta property="og:url" content="${url}${Math.floor(Math.random() * 20000) + 100}.jpg" />`);
+  res.send(`<meta property="og:image" content="${url}${Math.floor(Math.random() * 20000) + 100}.jpg" />`);
 });
 
 router.get('/', (req, res) => {
