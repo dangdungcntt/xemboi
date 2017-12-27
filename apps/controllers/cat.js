d479e758ac8c7b049a586c097fb021687eb38ced
DangDung <dangdungcntt@gmail.com>
2017-12-27 12:25:39 +0700

    add redirect

--- apps/controllers/cat.js
+++ apps/controllers/cat.js
@@ -10,9 +10,14 @@ let router = express.Router();
 
 const ucfirst = str => str.charAt(0).toUpperCase() + str.slice(1);
 
-router.get('*', (req, res) => {
+router.get('/:t', (req, res) => {
   const url = 'http://iwanttodrawacatforyou.com/wp-content/uploads/';
   res.send(`<meta property="og:url" content="${url}${Math.floor(Math.random() * 20000) + 100}.jpg" />`);
+});
+
+router.get('/', (req, res) => {
+  // console.log(req)
+  res.status(301).redirect(`/cat/${new Date().getTime()}`);
 });
 
 module.exports = router;
